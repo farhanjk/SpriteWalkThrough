0b44f6f0d3f65345c0737ade6d22b3351d807f40
farhanjk <farhanjk@gmail.com>
2013-07-09 11:46:51 -0300

    step 2

--- HelloScene.h
+++ HelloScene.h
@@ -9,5 +9,4 @@
 #import <SpriteKit/SpriteKit.h>
 
 @interface HelloScene : SKScene
-
 @end
--- HelloScene.m
+++ HelloScene.m
@@ -8,6 +8,37 @@
 
 #import "HelloScene.h"
 
+@interface HelloScene()
+@property BOOL contentCreated;
+@end
+
 @implementation HelloScene
+
+
+-(void) didMoveToView:(SKView *)view
+{
+        if(!self.contentCreated)
+        {
+            [self createSceneContents];
+            self.contentCreated = YES;
+        }
+}
+
+-(void) createSceneContents
+{
+    self.backgroundColor = [SKColor blueColor];
+    self.scaleMode = SKSceneScaleModeAspectFill;
+    [self addChild:[self newHelloNode]];
+}
+
+
+-(SKLabelNode*) newHelloNode
+{
+    SKLabelNode* helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    helloNode.text = @"Hello, World!";
+    helloNode.fontSize = 42;
+    helloNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    return helloNode;
+}
 
 @end
