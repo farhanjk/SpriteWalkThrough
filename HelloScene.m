94e934fa22c2d746a7d5a6ea5fe20a7264fe9f75
farhanjk <farhanjk@gmail.com>
2013-07-09 13:23:20 -0300

    Animation with lights

--- HelloScene.m
+++ HelloScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "HelloScene.h"
+#import "SpaceshipScene.h"
 
 @interface HelloScene()
 @property BOOL contentCreated;
@@ -35,10 +36,30 @@
 -(SKLabelNode*) newHelloNode
 {
     SKLabelNode* helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    helloNode.name = @"helloNode";
     helloNode.text = @"Hello, World!";
     helloNode.fontSize = 42;
     helloNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     return helloNode;
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    SKNode* helloNode = [self childNodeWithName:@"helloNode"];
+    if (helloNode!=nil) {
+        helloNode.name = nil;
+        SKAction* moveUp = [SKAction moveByX:0 y:100.0 duration:0.5];
+        SKAction* zoom = [SKAction scaleTo:2.0 duration:0.25];
+        SKAction* pause = [SKAction waitForDuration:0.5];
+        SKAction* fadeAway = [SKAction fadeOutWithDuration:0.25];
+        SKAction* remove = [SKAction removeFromParent];
+        SKAction* moveSequence = [SKAction sequence:@[moveUp, zoom, pause, fadeAway, remove]];
+        [helloNode runAction:moveSequence completion:^{
+            SKScene* spaceshipScene = [[SpaceshipScene alloc] initWithSize:CGSizeMake(768,1024)];
+            SKTransition* doors = [SKTransition doorsCloseVerticalWithDuration:0.5];
+            [self.view presentScene:spaceshipScene transition:doors];
+        }];
+    }
 }
 
 @end
---
+++ SpriteWalkThrough/SpaceshipScene.h
@@ -0,0 +1,13 @@
+//
+//  SpaceshipScene.h
+//  SpriteWalkThrough
+//
+//  Created by Farhan Khan on 2013-07-09.
+//  Copyright (c) 2013 Farhan Khan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SpaceshipScene : SKScene
+
+@end
